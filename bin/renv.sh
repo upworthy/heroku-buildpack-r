1ac5349e326fbb3e1df6fe3228f1dfb4b875cd94
VirtualStaticVoid <virtualstaticvoid@gmail.com>
2016-08-27 21:59:59 +0200

    tmp

--- bin/renv.sh
+++ bin/renv.sh
@@ -5,4 +5,5 @@ ARCH=`dpkg-architecture -qDEB_HOST_MULTIARCH`
 export TOOLS_DIR="/app/.tools"
 export FAKECHROOT_DIR="$TOOLS_DIR/fakechroot"
 export CHROOT_DIR="/app/.root"
+export LD_LIBRARY_PATH="$FAKECHROOT_DIR/lib/fakechroot:$LD_LIBRARY_PATH"
 export PATH="$FAKECHROOT_DIR/sbin:$FAKECHROOT_DIR/bin:$PATH"
